3cdb15d4b61f14d305f3a718c7fd831b4d01ec60
https://github.com/turnes <rafael@turnes.com.br>
2020-05-05 01:23:09 -0300

    Dockered the application. Minor change index.js.

--- desafio1/index.js
+++ desafio1/index.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Maratona Full Cycle 2.0'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Maratona FullCycle 2.0, App listen to http://localhost:${port}`))
